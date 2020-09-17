f8441a0185deb80ef54730c8f3026f8234dafa0e
christiano39 <carneson39@gmail.com>
2020-09-17 12:01:54 -0600

    refactor single_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,17 +2,28 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    counts = {}
-    for i in arr:
-        if i in counts:
-            counts[i] += 1
-        else:
-            counts[i] = 1
+# def single_number(arr):
+#     counts = {}
+#     for i in arr:
+#         if i in counts:
+#             counts[i] += 1
+#         else:
+#             counts[i] = 1
 
-    for num in counts:
-        if counts[num] == 1:
-            return num
+#     for num in counts:
+#         if counts[num] == 1:
+#             return num
+
+def single_number(arr):
+    s = set()
+
+    for num in arr:
+        if num in s:
+            s.remove(num)
+        else:
+            s.add(num)
+
+    return list(s)[0]
 
 
 if __name__ == '__main__':
