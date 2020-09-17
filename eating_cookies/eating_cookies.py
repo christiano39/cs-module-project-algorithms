5174ab00a2619f7321e2455497a2444d822c58bc
christiano39 <carneson39@gmail.com>
2020-09-17 11:57:58 -0600

    added cache to eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,15 +2,19 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    if n <= 1:
+def eating_cookies(n, cache=None):
+    if n < 0:
+        return 0
+    elif n == 0:
         return 1
-    elif n == 2:
-        return 2
-    elif n == 3:
-        return 4
+    elif cache and cache[n] > 0:
+        return cache[n]
+    else:
+        if not cache:
+            cache = [0 for i in range(n+1)]
+        cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
     
-    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    return cache[n]
 
     
 
